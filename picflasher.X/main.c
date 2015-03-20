5d08087df13d4e80e415243bb523677add437a97
lobsteropteryx <ian.firkin@gmail.com>
2015-03-19 21:54:57 -0700

    initial commit with working program

--- picflasher.X/main.c
+++ picflasher.X/main.c
@@ -1,17 +1,37 @@
 /* 
  * File:   main.c
- * Author: Ian
+ * Author: Ian Firkin
  *
- * Created on February 14, 2015, 7:29 PM
+ * Created on March 19, 2015, 7:44 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <xc.h>
 
-/*
- * 
- */
+/* Set Configuration bits */
+#pragma config FOSC = INTOSCIO
+#pragma config WDTE = OFF
+#pragma config MCLRE = OFF
+
+/* Define oscillator frequency for delays */
+#define _XTAL_FREQ 8000000
+
 int main(int argc, char** argv) {
+
+    /* Set Analog pins to digital i/o */
+    CMCON0 = 0b00000100;
+    ANSEL = 0b00000000;
+
+    /* Set port RC2 to output */
+    TRISC = 0b11111011;
+
+    while (1) {
+        RC2 = 1;
+        __delay_ms(500);
+        RC2 = 0;
+        __delay_ms(500);
+    }
 
     return (EXIT_SUCCESS);
 }
