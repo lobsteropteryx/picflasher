6a1bbba5cc022786e7afa7a78d7f96ae452193c2
lobsteropteryx <ian.firkin@gmail.com>
2015-03-22 20:58:01 -0700

    Adds support for pushbutton interrupt, and some refactoring

--- picflasher.X/main.c
+++ picflasher.X/main.c
@@ -10,12 +10,35 @@
 #include <xc.h>
 
 /* Set Configuration bits */
-#pragma config FOSC = INTOSCIO
-#pragma config WDTE = OFF
-#pragma config MCLRE = OFF
+#pragma config FOSC = INTOSCIO /* Use high speed internal oscillator */
+#pragma config WDTE = OFF /* Disable Watchdog timer */
+#pragma config MCLRE = OFF /* Disable Master Clear */
 
 /* Define oscillator frequency for delays */
 #define _XTAL_FREQ 8000000
+
+volatile int keyPressed = 0;
+
+/* This is triggered by the interrupt on change for PORTA, pin 5.
+ When the interrupt is fired, the chip will wake from sleep. */
+void __interrupt handleButtonPress(void) {
+    int portA;
+    if (INTCONbits.RAIF == 1) { // Interrupt bit set
+//        portA = PORTA;
+        INTCONbits.RAIF = 0; // Clear it
+        keyPressed = 1; // Set our flag and get out of here
+    }
+}
+
+void flashLED(void) {
+    int i;
+    for (i = 0; i < 3; i++) {
+        RC2 = 1;
+        __delay_ms(100);
+        RC2 = 0;
+        __delay_ms(100);
+    }
+}
 
 int main(int argc, char** argv) {
 
@@ -23,16 +46,27 @@ int main(int argc, char** argv) {
     CMCON0 = 0b00000100;
     ANSEL = 0b00000000;
 
+    OPTION_REG = 0b00000000; /* Enable PORTA pullups */
+    TRISA = 0b11111111; /* Set input / output for PORTA */
+    WPUA5 = 1; /* Enable internal pullup for A5 */
+    IOCA5 = 1; /* Enable interrupt on change for A5 */
+
     /* Set port RC2 to output */
     TRISC = 0b11111011;
 
+    /* Enable Interrupts*/
+    INTCONbits.GIE = 1;
+    INTCONbits.RAIE = 1;
+    RC2 = 0; /* Turn LED off */
     while (1) {
-        RC2 = 1;
-        __delay_ms(500);
-        RC2 = 0;
-        __delay_ms(500);
+        SLEEP();
+        if (keyPressed) {
+            keyPressed = 0;
+            flashLED();
+        }
     }
 
     return (EXIT_SUCCESS);
 }
+
 
